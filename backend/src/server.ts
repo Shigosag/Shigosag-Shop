f09826c8e15b0ff4aec491622686c8958de85e70
Shigosag (Segun Arulogun Gabriel) <88502810+Shigosag@users.noreply.github.com>
2026-07-22 10:04:52 +0000

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,8 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +18,10 @@ app.use("/uploads", express.static("public/uploads"));
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
-  console.log("🚀 Shigosag API Running");
+app.listen(PORT, () => {
+  console.log(`🚀 Shigosag API Running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Shigosag API Online");
 });
